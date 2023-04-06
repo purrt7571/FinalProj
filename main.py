dbead5aff2109ef885b7160da78a30c8e1b24414
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 00:00:40 +0800

    Basic GUI Layout for Graph

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 # Tk main window
 root = tk.Tk()
 root.title("Vector Addition")
+frame = tk.Frame(root)
+frame.grid(row=0,column=1)
 
 # Setup Graph elements
 fig = Figure()
-canvas = FigureCanvasTkAgg(fig, root)
-toolbar = NavigationToolbar2Tk(canvas, root)
+subplt = fig.add_subplot(111)
+canvas = FigureCanvasTkAgg(figure = fig, master = frame)
+canvas.get_tk_widget().grid(row=0, column=0)
+toolbar = NavigationToolbar2Tk(canvas = canvas, window = frame, pack_toolbar = False)
+toolbar.grid(row=1,column=0)
+
+# Run GUI
+root.mainloop()
