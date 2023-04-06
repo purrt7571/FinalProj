cd14e58ac2dfc22c33a46ac8e96ba8b0855a0106
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-06 22:42:40 +0800

    Import important modules

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import numpy as np
+import tkinter as tk
+from matplotlib.figure import Figure
+from matplotlib.backends._backend_tk import NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Test line
 print(np.hypot(5,3))
