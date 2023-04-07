6ed08270c4373faa3505751de5e9916bca93006a
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 19:43:56 +0800

    Added "Add Vector" Panel (nonfunctional)

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def on_close() -> None:
     root.destroy()
     return
 
+def add_vector():
+    pass
+
 
 # Initialize dict[np.array] for vectors
 vct: dict[np.array] = {}
@@ -25,15 +28,50 @@ root.grid_columnconfigure(2, weight=1)
 root.grid_rowconfigure(0, weight=1)
 
 # Frame/Container for buttons/labels/textboxes/checkboxes
-control_frame = tk.Frame(root, background="red")
+control_frame = tk.Frame(root)
 control_frame.grid(column=0, row=0, sticky='nsew')
+control_frame.grid_columnconfigure(0, weight=1)
+control_frame.grid_columnconfigure(1, weight=0)
+control_frame.grid_rowconfigure(0, weight=0)
+control_frame.grid_rowconfigure(1, weight=0)
+
+# Setup frame for adding vectors to plot
+add_vector_frame = ttk.Labelframe(control_frame, text="Vector")
+add_vector_frame.grid(column=0, row=0, sticky="new", padx=10, pady=10)
+add_vector_frame.grid_columnconfigure(0,weight=1)
+add_vector_frame.grid_columnconfigure(1,weight=3)
+add_vector_frame.grid_columnconfigure(2,weight=1)
+add_vector_frame.grid_columnconfigure(3,weight=3)
+add_vector_frame.grid_rowconfigure(0, weight=0)
+add_vector_frame.grid_rowconfigure(1, weight=0)
+add_vector_frame.grid_rowconfigure(2, weight=0)
+
+name_label = ttk.Label(add_vector_frame, text="Vector name: ")
+name_label.grid(column=0, row=0, sticky="nw", padx=10)
+name_txtbox = ttk.Entry(add_vector_frame)
+name_txtbox.grid(column=1, row=0, columnspan=3, sticky="new", padx=10)
+
+requirement1_label = ttk.Label(add_vector_frame, text="X: ")
+requirement2_label = ttk.Label(add_vector_frame, text="Y: ")
+requirement1_label.grid(column=0, row=1, sticky="ew", padx=10)
+requirement2_label.grid(column=2, row=1, sticky="ew", padx=10)
+requirement1_txtbox = ttk.Entry(add_vector_frame)
+requirement2_txtbox = ttk.Entry(add_vector_frame)
+requirement1_txtbox.grid(column=1, row=1, sticky="ew", padx=10, pady=10)
+requirement2_txtbox.grid(column=3, row=1, sticky="ew", padx=10, pady=10)
+
+add_vector_button = ttk.Button(add_vector_frame, text="Add Vector", command=add_vector)
+add_vector_button.grid(column=0, row=2, columnspan=4, sticky="ew", pady=5, padx=10)
+
+scroll = tk.Scrollbar(control_frame, orient="vertical")
+scroll.grid(column=1, row=0, rowspan=1, sticky="nse")
 
 # Display vector values
 tree = ttk.Treeview(root, columns=("c1", "c2", "c3"), show="headings")
 tree.grid(column=1, row=0, sticky="nsw")
 tree.column("# 1", width=70)
-tree.column("# 2", width=100)
-tree.column("# 3", width=100)
+tree.column("# 2", width=100, anchor="e")
+tree.column("# 3", width=100, anchor="e")
 tree.heading("# 1", text="Name")
 tree.heading("# 2", text="X")
 tree.heading("# 3", text="Y")
