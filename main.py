ac7cffe47a9a7c425f442c1399c6a694fd6163ef
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 12:49:06 +0800

    Changed from Grid manager to Pack manager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tkinter as tk
-from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -9,16 +9,16 @@ root = tk.Tk()
 root.title("Vector Addition")
 
 # Frame/Container for Graph
-frame = tk.Frame(root)
-frame.grid(row=0,column=1)
+frame = tk.Frame(root, background="green")
+frame.pack(side="right", fill="both")
 
 # Setup Graph elements
-fig = Figure()
-subplt = fig.add_subplot(111)
-canvas = FigureCanvasTkAgg(figure = fig, master = frame)
-canvas.get_tk_widget().grid(row=0, column=0)
+fig = plt.figure()
+subplt = fig.add_axes((0.1,0.1,0.8,0.8))
+canvas = FigureCanvasTkAgg(figure=fig, master=frame)
+canvas.get_tk_widget().pack(side='top', fill='both')
 toolbar = NavigationToolbar2Tk(canvas = canvas, window = frame, pack_toolbar = False)
-toolbar.grid(row=1,column=0)
+toolbar.pack(side='bottom', fill='x')
 
 # Run GUI
 root.mainloop()
