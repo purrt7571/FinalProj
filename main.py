081ade2da03a6893cb936288c56997dd7813670f
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 20:02:01 +0800

    Added radio buttons for coordinate choice

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def add_vector():
     pass
 
 
-# Initialize dict[np.array] for vectors
+# Initialize dict[np.array] for vectors and resultant for sum
 vct: dict[np.array] = {}
+resultant: np.array = np.array([0,0])
 
 # Tk main window
 root = tk.Tk()
@@ -24,7 +25,7 @@ root.title("Vector Addition")
 root.minsize(1500, 900)
 root.grid_columnconfigure(0, weight=1)
 root.grid_columnconfigure(1, weight=0)
-root.grid_columnconfigure(2, weight=1)
+root.grid_columnconfigure(2, weight=3)
 root.grid_rowconfigure(0, weight=1)
 
 # Frame/Container for buttons/labels/textboxes/checkboxes
@@ -45,14 +46,15 @@ add_vector_frame.grid_columnconfigure(3,weight=3)
 add_vector_frame.grid_rowconfigure(0, weight=0)
 add_vector_frame.grid_rowconfigure(1, weight=0)
 add_vector_frame.grid_rowconfigure(2, weight=0)
+add_vector_frame.grid_rowconfigure(3, weight=0)
 
 name_label = ttk.Label(add_vector_frame, text="Vector name: ")
 name_label.grid(column=0, row=0, sticky="nw", padx=10)
 name_txtbox = ttk.Entry(add_vector_frame)
 name_txtbox.grid(column=1, row=0, columnspan=3, sticky="new", padx=10)
 
-requirement1_label = ttk.Label(add_vector_frame, text="X: ")
-requirement2_label = ttk.Label(add_vector_frame, text="Y: ")
+requirement1_label = ttk.Label(add_vector_frame, text="X / R : ")
+requirement2_label = ttk.Label(add_vector_frame, text="Y / \u03B8 : ")
 requirement1_label.grid(column=0, row=1, sticky="ew", padx=10)
 requirement2_label.grid(column=2, row=1, sticky="ew", padx=10)
 requirement1_txtbox = ttk.Entry(add_vector_frame)
@@ -60,8 +62,15 @@ requirement2_txtbox = ttk.Entry(add_vector_frame)
 requirement1_txtbox.grid(column=1, row=1, sticky="ew", padx=10, pady=10)
 requirement2_txtbox.grid(column=3, row=1, sticky="ew", padx=10, pady=10)
 
+coordinate_choice = tk.IntVar(value=0)
+
+cartesian_radio = ttk.Radiobutton(add_vector_frame, text="Cartesian", variable=coordinate_choice, value=0)
+cartesian_radio.grid(column=0, row=2, columnspan=2, sticky="ew", padx=10, pady=10)
+polar_radio = ttk.Radiobutton(add_vector_frame, text="Polar", variable=coordinate_choice, value=1)
+polar_radio.grid(column=2, row=2, columnspan=2, sticky="ew", padx=10, pady=10)
+
 add_vector_button = ttk.Button(add_vector_frame, text="Add Vector", command=add_vector)
-add_vector_button.grid(column=0, row=2, columnspan=4, sticky="ew", pady=5, padx=10)
+add_vector_button.grid(column=0, row=3, columnspan=4, sticky="ew", pady=(5,10), padx=10)
 
 scroll = tk.Scrollbar(control_frame, orient="vertical")
 scroll.grid(column=1, row=0, rowspan=1, sticky="nse")
