fd8a8240c507a1f018d4772118a302fc4ea747b9
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 18:34:16 +0800

    Added Vector Values Viewer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tkinter as tk
+import tkinter.ttk as ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -17,12 +18,21 @@ vct: dict[np.array] = {}
 # Tk main window
 root = tk.Tk()
 root.title("Vector Addition")
-root.minsize(1200, 800)
+root.minsize(1500, 900)
 
 # Frame/Container for buttons/labels/textboxes/checkboxes
 control_frame = tk.Frame(root)
-control_frame.pack(side='left', fill='both', expand=True)
+control_frame.pack(side='left', fill='y', expand=True)
 
+# TreeView for Vector Values
+tree = ttk.Treeview(root, columns=("c1", "c2", "c3"), show="headings")
+tree.pack(side='left', fill='both', expand=True)
+tree.column("# 1", width=50)
+tree.column("# 2", width=50)
+tree.column("# 3", width=50)
+tree.heading("# 1", text="Name")
+tree.heading("# 2", text="X")
+tree.heading("# 3", text="Y")
 
 
 # Frame/Container for Graph
