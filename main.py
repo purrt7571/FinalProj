3a9c3aa7048617292ed400673572af4885bc0bc9
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 13:34:17 +0800

    Finalized Scaling Feature

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Close matplotlib figure then close program
-def on_close():
+def on_close() -> None:
     plt.close(fig)
     root.destroy()
+    return
+
+
+# Initialize dict[np.array] for vectors
+vct: dict[np.array] = {}
 
 # Tk main window
 root = tk.Tk()
@@ -15,14 +20,13 @@ root.title("Vector Addition")
 
 # Frame/Container for Graph
 canvas_frame = tk.Frame(root, background="green")
-canvas_frame.pack(side="right", fill="both")
-canvas_frame.pack_propagate(True)
+canvas_frame.pack(side="right", fill="both", expand=True)
 
 # Setup Graph elements
 fig = plt.figure()
 plot = fig.add_subplot()
 canvas = FigureCanvasTkAgg(figure=fig, master=canvas_frame)
-canvas.get_tk_widget().pack(side='top', fill='both')
+canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
 toolbar = NavigationToolbar2Tk(canvas = canvas, window = canvas_frame, pack_toolbar = False)
 toolbar.pack(side='bottom', fill='x')
 
