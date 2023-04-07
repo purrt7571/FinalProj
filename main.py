ebd5b313d07e2b808d99600553631cbaaa70dccc
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 18:42:13 +0800

    Added TreeView

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tkinter as tk
+import tkinter.ttk as ttk
 import matplotlib.pyplot as plt
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -17,18 +18,29 @@ vct: dict[np.array] = {}
 # Tk main window
 root = tk.Tk()
 root.title("Vector Addition")
-root.minsize(1200, 800)
+root.minsize(1500, 900)
 root.grid_columnconfigure(0, weight=1)
-root.grid_columnconfigure(1, weight=1)
+root.grid_columnconfigure(1, weight=0)
+root.grid_columnconfigure(2, weight=1)
 root.grid_rowconfigure(0, weight=1)
 
 # Frame/Container for buttons/labels/textboxes/checkboxes
 control_frame = tk.Frame(root, background="red")
 control_frame.grid(column=0, row=0, sticky='nsew')
 
+# Display vector values
+tree = ttk.Treeview(root, columns=("c1", "c2", "c3"), show="headings")
+tree.grid(column=1, row=0, sticky="nsw")
+tree.column("# 1", width=70)
+tree.column("# 2", width=100)
+tree.column("# 3", width=100)
+tree.heading("# 1", text="Name")
+tree.heading("# 2", text="X")
+tree.heading("# 3", text="Y")
+
 # Frame/Container for Graph
 canvas_frame = tk.Frame(root, background="blue")
-canvas_frame.grid(column=1, row=0, sticky="nsew")
+canvas_frame.grid(column=2, row=0, sticky="nsew")
 canvas_frame.grid_rowconfigure(0, weight=1)
 canvas_frame.grid_rowconfigure(1, weight=0)
 canvas_frame.grid_columnconfigure(0, weight=1)
