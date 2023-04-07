d6bf9ba4f49227b68ffd35ecd14deabb6e9d9b72
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 14:35:08 +0800

    Fixed Grid Manager

--- main.py
+++ main.py
@@ -18,24 +18,28 @@ vct: dict[np.array] = {}
 root = tk.Tk()
 root.title("Vector Addition")
 root.minsize(1200, 800)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_rowconfigure(0, weight=1)
 
 # Frame/Container for buttons/labels/textboxes/checkboxes
-control_frame = tk.Frame(root)
-control_frame.pack(side='left', fill='both', expand=True)
-
-
+control_frame = tk.Frame(root, background="red")
+control_frame.grid(column=0, row=0, sticky='nsew')
 
 # Frame/Container for Graph
-canvas_frame = tk.Frame(root)
-canvas_frame.pack(side="right", fill="both", expand=True)
+canvas_frame = tk.Frame(root, background="blue")
+canvas_frame.grid(column=1, row=0, sticky="nsew")
+canvas_frame.grid_rowconfigure(0, weight=1)
+canvas_frame.grid_rowconfigure(1, weight=0)
+canvas_frame.grid_columnconfigure(0, weight=1)
 
 # Setup Graph elements
 fig = plt.figure()
 plot = fig.add_subplot()
 canvas = FigureCanvasTkAgg(figure=fig, master=canvas_frame)
-canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
+canvas.get_tk_widget().grid(column=0, row=0, sticky='nsew')
 toolbar = NavigationToolbar2Tk(canvas = canvas, window = canvas_frame, pack_toolbar = False)
-toolbar.pack(side='bottom', fill='x')
+toolbar.grid(column=0, row=1, sticky='sew')
 
 # Set closing sequence
 root.protocol("WM_DELETE_WINDOW", on_close)
