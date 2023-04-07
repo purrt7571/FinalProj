3ee401925488eb0c5830755a2fe8eb935613df51
purrt7571 <patrickmontenegro17@gmail.com>
2023-04-07 13:27:08 +0800

    Implement closing sequence

--- main.py
+++ main.py
@@ -4,21 +4,30 @@ import matplotlib.pyplot as plt
 from matplotlib.backends._backend_tk import NavigationToolbar2Tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+# Close matplotlib figure then close program
+def on_close():
+    plt.close(fig)
+    root.destroy()
+
 # Tk main window
 root = tk.Tk()
 root.title("Vector Addition")
 
 # Frame/Container for Graph
-frame = tk.Frame(root, background="green")
-frame.pack(side="right", fill="both")
+canvas_frame = tk.Frame(root, background="green")
+canvas_frame.pack(side="right", fill="both")
+canvas_frame.pack_propagate(True)
 
 # Setup Graph elements
 fig = plt.figure()
-subplt = fig.add_axes((0.1,0.1,0.8,0.8))
-canvas = FigureCanvasTkAgg(figure=fig, master=frame)
+plot = fig.add_subplot()
+canvas = FigureCanvasTkAgg(figure=fig, master=canvas_frame)
 canvas.get_tk_widget().pack(side='top', fill='both')
-toolbar = NavigationToolbar2Tk(canvas = canvas, window = frame, pack_toolbar = False)
+toolbar = NavigationToolbar2Tk(canvas = canvas, window = canvas_frame, pack_toolbar = False)
 toolbar.pack(side='bottom', fill='x')
+
+# Set closing sequence
+root.protocol("WM_DELETE_WINDOW", on_close)
 
 # Run GUI
 root.mainloop()
